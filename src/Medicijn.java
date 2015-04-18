04b1921ca628c9f0c483b0110a4d71f829191a39
MatsDeMeyer <matsdemeyer@gmail.com>
2015-04-18 16:08:14 +0200

    enkel nog wijzigplaatsbepaling in medicijn uit te werken

--- src/Medicijn.java
+++ src/Medicijn.java
@@ -6,29 +6,54 @@ public class Medicijn {
 	String stofnaam;
 	int aantal;
 	int gewensteAantal;
-	int minimaAantal;
+	int minimumAantal;
 	String fabrikant;
 	int prijs;
-	int kastID;
+	int kastID;	
+	String houdbaarheid;		//String die de datum voorstelt in het formaat "dd-MM-yyyy' zoals sdf.
+	
 
-	public static void controleerOpAantal()
+	public static int controleerOpAantal()
 	{
-		return aantal;
+		//return 0 als er genoeg zijn, en anders hoeveel er te weinig zijn.
+		if (aantal =< minimumAantal)
+		{
+			return gewensteAantal-aantal;
+		}
+		
+		else
+		{
+			return 0;
+		}
+		
 	}
 	
-	public static void controleerOpHoudbaarheid()
+	public static boolean controleerOpHoudbaarheid()
 	{
 		Date huidig = new Date();
-	}
-	
-	public static void wijzigMerknaam(merknaam)
-	{
+		SimpleDateFormat sdf = new SimpledateFormat("dd-MM-yyyy");
+		Date houdbaarheidsDatum = sdf.format(houdbaarheid);
+		Date huidigD = sdf.format(huidig)
+		
+		if (huidigD.after(houdbaarheidsDatum))
+		{
+			return false;	//niet meer houdbaar als de huidige datum later is dan de houdbaarheidsdatum
+		}
+		else 	
+		{
+			return true;	//in alle andere gevallen wel houdbaar
+		}
 		
 	}
 	
-	public static void wijzigStofnaam(stofnaam)
+	public static void wijzigMerknaam(String newMerknaam)
 	{
-		
+		merknaam = newMerknaam;	
+	}
+	
+	public static void wijzigStofnaam(String newStofnaam)
+	{
+		stofnaam = newStofnaam;	
 	}
 	
 	public static void wijzigPlaatsBepaling(kastID: Kast)
@@ -36,34 +61,34 @@ public class Medicijn {
 		
 	}
 	
-	public static void wijzigAantal(aantal)
+	public static void wijzigAantal(int newAantal)
 	{
-		
+		aantal = newAantal;
 	}
 	
-	public static void wijzigGewensteAantal(aantal)
+	public static void wijzigGewensteAantal(int newAantal)
 	{
-		
+		gewensteAantal = newAantal;
 	}
 	
-	public static void wijzigMinimaAantal(aantal)
+	public static void wijzigMinimumAantal(int newAantal)
 	{
-		
+		minimumAantal = newAantal;
 	}
 	
-	public static void wijzigFabrikant(fabrikant)
+	public static void wijzigFabrikant(String newFabrikant)
 	{
-		
+		fabrikant = newFabrikant;
 	}
 	
-	public static void wijzigPrijs(prijs)
+	public static void wijzigPrijs(int newPrijs)
 	{
-		
+		prijs = newPrijs
 	}
 	
-	public static void vermeerderMedicijnAantal()
+	public static void vermeerderMedicijnAantal(int vermeerder)
 	{
-		
+		aantal = aantal + vermeerder;
 	}
 	
 }
