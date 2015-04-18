63061f0328c6283361d1b3e61e31e9428a67068c
abdil <abdil@abdil-pc>
2015-04-18 16:01:10 +0200

    Klasse Bestelling commit 1

--- src/Bestelling.java
+++ src/Bestelling.java
@@ -1,10 +1,20 @@
+/* Bedenkingen en toevoegingen
+ * 
+ * bestellingsStatus:
+ *  - 1: Nog niet besteld, aanvulbaar
+ *  - 2: Besteld
+ *  - 3: Ontvangen
+ * 
+ * Toevoegingen:
+ * 	- bestellingsDatum
+ *  - 
+ */
 
 public class Bestelling {
 	
 	int bestellingsID;
-	boolean[] isGeleverd = new boolean [];
-	boolean[] isBesteld = new boolean[];
-	int[] medicijnID = new int[];
+	int bestellingsStatus=1;
+	int[] medicijnID = new int;
 	int[] aantal = new int[];
 	int[] prijs  = new int[];
 	Medicijn[] medicijnID = new Medicijn[];
@@ -17,7 +27,11 @@ public class Bestelling {
 
 	public static void bevestigBestelling()
 	{
-		
+		if(bestellingsStatus==2)
+			bestellingsStatus=3;
+		else
+			System.out.println("Bestelling is nog niet besteld. Gelieve uw bestellingen pas na ontvangst te bevestigen.");
+
 	}
 	
 	public static void vermeerderMedicijnAantal(medicijnID*[]: Medicijn)
