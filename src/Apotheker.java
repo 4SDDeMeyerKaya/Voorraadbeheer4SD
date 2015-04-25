61c14ca97c0cce784dc76db5954f46fd62b10bf8
MatsDeMeyer <matsdemeyer@gmail.com>
2015-04-25 19:36:55 +0200

    uihgrjo

--- src/Apotheker.java
+++ src/Apotheker.java
@@ -2,24 +2,15 @@
 public class Apotheker {
 	
 	
-	public static void verkoopMedicijn( medicijnID, aantal, kastID)
-	{
+	//public static void verkoopMedicijn(medicijnID, aantal, kastID)
+	//{
 		
+	//}
+	
+	public void bevestigBestelling(Bestelling.getID())
+	{
+			
 	}
 	
-	public static void bevestigBestelling(Bestelling)
-	{
-		
-	}
-	
-	public void geefKastInhoud(int kastID)
-	{
-		System.out.println("Items in kast" + kastID + ": \n");
-		if (Medicijn.geefKastID() == kastID)
-		{
-			System.out.println(Medicijn.geefMedicijnID() + "\n")
-		}		
-		
-	}
 }
 
