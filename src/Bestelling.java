b4e7965d49865b0e8c2afa7c65a9c0bfaaec278e
abkaya <abdil.kaya@student.uantwerpen.be>
2015-04-26 12:00:42 +0200

    Bestelling en Voorraadbeheer opslag algoritmes

--- src/Bestelling.java
+++ src/Bestelling.java
@@ -1,73 +1,33 @@
-/* Bestelling
+/* 
+ * Bestelling onderhoudt een lijst met objecten van BestelMedicijn. Een lijst met objecten van Bestelling wordt
+ * hogerop door Voorraadbeheer onderhouden.
+ * Op deze manier vermijden we een te complexe structuur binnenin de verschillende klassen.
  */
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Bestelling {
 	
-	// Klassevariabele voor het aantal bestellingen geinstantieerd [bron: 2]
-	// refereren hiernaar gebeurt met de klassenaam als referentie: Bestelling.aantalBestellingen
-	
-    private static int aantalBestellingen = 0; 	//aantal objecten 
-	private int bestellingsStatus=1;
-	private int[] aantalTeBestellen; 			//aantal te bestellen van een item
-	private int itemNummer; 					//nummer van het item binnen een bestellingsObject
-	private int[] prijs;
-	private Medicijn medicijnNaam;
-	private int id;
-	static Scanner reader = new Scanner(System.in);
-	
+	ArrayList<BestelMedicijn> besmedlist = new ArrayList<BestelMedicijn>();
+	private boolean isBesteld;
 	
 	public Bestelling(){
-        bestellingsStatus=1;
-        itemNummer=0;
-        //test change
+		isBesteld=false;
     }
 	
-	public void voegItemToe(){
-		itemNummer=itemNummer+1;
-		aantalTeBestellen[itemNummer]=nieuwAantalTeBestellen;
-		prijs[itemNummer]=nieuwPrijs;
-		
+	//voegMedicijnToe voegt een nieuw object van BestelMedicijn toe aan de lijst.
+	public void voegMedicijnToe(String merknaam, int aantal, int prijs){
+		if (besmedlist.contains(merknaam))
+			System.out.println("Let op! Dit medicijn heeft u al eerder toegevoegd.");
+		else
+			this.besmedlist.add(new BestelMedicijn(merknaam, aantal, prijs));
 	}
 	
-	public void bestelBestelling()
-	{
-		if(bestellingsStatus==1)
-			bestellingsStatus=2;
-		if(bestellingsStatus==2)
-			System.out.println("Bestelling is reeds gemaakt.");
-		if(bestellingsStatus==3)
-			System.out.println("Bestelling is al bevestigd.");
+
+	public boolean isBesteld() {
+		return isBesteld;
 	}
 
-	public void bevestigBestelling()
-	{
-		if(bestellingsStatus==2)
-			bestellingsStatus=3;
-		if(bestellingsStatus==1)
-			System.out.println("Bestelling is nog niet besteld. Gelieve uw bestellingen pas na ontvangst te bevestigen.");
-		if(bestellingsStatus==3)
-			System.out.println("Bestelling is al bevestigd.");
+	public void setBesteld(boolean isBesteld) {
+		this.isBesteld = isBesteld;
 	}
-	
-	public static void vermeerderMedicijnAantal(Medicijn medicijnID)
-	{
-		
-	}
-	
-	public static void bestellingLogWrite(String logLines)
-	{
-		
-	}
-	
-	public static void controleerOpAantal(Medicijn BestellingsID)
-	{
-		
-	}
-	
-	// new method to return the ID instance variable
-    public int getID() {
-        return id;
-    }
 }
--- src/Voorraadbeheer.java
+++ src/Voorraadbeheer.java
@@ -5,71 +5,51 @@
  * Het geeft wel vele voordelen zoals bijvoorbeeld iteratief een lijst van objecten
  * te doorlopen.
  */
-import java.util.*;
+import java.util.ArrayList;
 
 public class Voorraadbeheer {
 
-	ArrayList<Medicijn> medlist = new ArrayList<Medicijn>();
-	ArrayList<Bestelling> beslist = new ArrayList<Bestelling>();
-	ArrayList<BestelMedicijn> besmedlist = new ArrayList<BestelMedicijn>();
+	static ArrayList<Medicijn> medlist = new ArrayList<Medicijn>();
+	static ArrayList<Bestelling> beslist = new ArrayList<Bestelling>();
 	
 	public Voorraadbeheer(){
 
 	}
 	
-	
 	//voegMedicijnToe voegt een nieuw object van Medicijn toe aan de lijst.
 	public void voegMedicijnToe(String merknaam, String stofnaam, int aantal, int gewensteAantal, int minimaAantal, String fabrikant, int prijs, int kast, String houdbaarheid){
-		this.medlist.add(new Medicijn(merknaam, stofnaam, aantal, gewensteAantal, minimaAantal, fabrikant, prijs, kast, houdbaarheid));
+		if (medlist.contains(merknaam))
+			System.out.println("Let op! Dit medicijn heeft u al eerder toegevoegd.");
+		else
+			Voorraadbeheer.medlist.add(new Medicijn(merknaam, stofnaam, aantal, gewensteAantal, minimaAantal, fabrikant, prijs, kast, houdbaarheid));
 	}
 	
 	//verwijderMedicijn zoekt naar overeenkomstige merknamen (in principe één hit max) en verwijdert dit object.
 	public void verwijderMedicijn(String merknaam){ 
-		for(int i=0; i<this.medlist.size();i++){
-			if (this.medlist.get(i).merknaam.equalsIgnoreCase(merknaam))
-				this.medlist.remove(i);
+		for(int i=0; i<Voorraadbeheer.medlist.size();i++){
+			if (Voorraadbeheer.medlist.get(i).merknaam.equalsIgnoreCase(merknaam))
+				Voorraadbeheer.medlist.remove(i);
 			}
 	}
 	
 	//voegBestellijstToe voegt een nieuw object van Bestelling toe aan de lijst.
 	public void voegBestellingToe(){
-		this.beslist.add(new Bestelling());
+		Voorraadbeheer.beslist.add(new Bestelling());
 	}
+	
+	//voegBestelMed
 	
 	
 	
 	public static void main(String args[]){
 		
 		Voorraadbeheer vb = new Voorraadbeheer(); 
+		vb.voegBestellingToe();
+		beslist.get(0).voegMedicijnToe("Supradyne", 10, 20);
+		beslist.get(0).voegMedicijnToe("Aspirine", 11, 21);
+		System.out.println("Bestelling ontvangen?: "+beslist.get(0).isBesteld());
+		System.out.println("merknaam: " + beslist.get(0).besmedlist.get(0).merknaam  +" aantal: "+ beslist.get(0).besmedlist.get(0).aantal +" prijs: "+ beslist.get(0).besmedlist.get(0).prijs);
+		System.out.println("merknaam: " + beslist.get(0).besmedlist.get(1).merknaam  +" aantal: "+ beslist.get(0).besmedlist.get(1).aantal +" prijs: "+ beslist.get(0).besmedlist.get(1).prijs);
 		
-/*
-		Scanner input = new Scanner(System.in);
-        System.out.println("HOEVEEL MEDICIJNEN WIL JE TOEVOEGEN?? (Test met 2+): ");
-        int testAantal = input.nextInt();
-		
-        
-        for(int k=0;k<testAantal;k++){
-        	
-        	System.out.println("merknaam: ");
-        	String merknaam = input.nextLine();
-        	System.out.println("stofnaam: ");
-        	String stofnaam = input.nextLine();
-        	System.out.println("fabrikant: ");
-        	String fabrikant = input.nextLine();
-        	System.out.println("houdbaarheid: ");
-        	String houdbaarheid = input.nextLine();
-        	System.out.println("aantal: ");
-        	int aantal = input.nextInt();
-        	System.out.println("gewenste aantal: ");
-        	int gewensteAantal = input.nextInt();
-        	System.out.println("minima aantal: ");
-        	int minimaAantal = input.nextInt();
-        	System.out.println("prijs: ");
-        	int prijs = input.nextInt();
-        	System.out.println("kast: ");
-        	int kast = input.nextInt();
 	}
-*/
-		
-}
 }
