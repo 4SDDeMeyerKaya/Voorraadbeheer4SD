7b07a5d01e2aa9c3243562b8678c23618dfed355
abkaya <abdil.kaya@student.uantwerpen.be>
2015-05-05 18:24:52 +0200

    Main test leeggehaald

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,6 @@ public class Main {
 					GUI window = new GUI();
 					window.frame.setVisible(true);
 					Voorraadbeheer.voegBestellingToe();
-					Voorraadbeheer.beslist.get(0).voegMedicijnToe("Supradyne", 10, 20);
-					Voorraadbeheer.beslist.get(0).voegMedicijnToe("Aspirine", 9001, 9001);
-					Voorraadbeheer.beslist.get(0).voegMedicijnToe("Aspirine", 11, 21);
-					Voorraadbeheer.verwijderMedicijn("probeer");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
