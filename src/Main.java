867afee129e6936117af1630d0d3fd6585c3d7d6
abkaya <abdil.kaya@student.uantwerpen.be>
2015-05-05 17:50:49 +0200

    Bestelalgoritme double output fix

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 	public static void main(String args[]){
 		final Voorraadbeheer vb = new Voorraadbeheer(); 
-		
-		vb.voegBestellingToe();
+	
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					GUI window = new GUI();
 					window.frame.setVisible(true);
+					Voorraadbeheer.voegBestellingToe();
 					Voorraadbeheer.beslist.get(0).voegMedicijnToe("Supradyne", 10, 20);
 					Voorraadbeheer.beslist.get(0).voegMedicijnToe("Aspirine", 9001, 9001);
 					Voorraadbeheer.beslist.get(0).voegMedicijnToe("Aspirine", 11, 21);
-					vb.verwijderMedicijn("probeer");
+					Voorraadbeheer.verwijderMedicijn("probeer");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
