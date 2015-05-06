4510f0a7adcb817842beb3d0c1738467ecc8c6e7
abkaya <abdil.kaya@student.uantwerpen.be>
2015-05-06 03:33:56 +0200

    Medicijn TABLE - Natuurlijk lukt het 1 minuut voor het slapengaan lol

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,23 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 	public static void main(String args[]){
 
-		Voorraadbeheer.voegBestellingToe();
-		Voorraadbeheer.medlist.add(new Medicijn("a","a",30,60,31,"a",30,12,"06-06-2015"));
-		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					GUI window = new GUI();
 					window.frame.setVisible(true);
+					Voorraadbeheer.voegBestellingToe();
+					Voorraadbeheer.medlist.add(new Medicijn("A","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("B","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("C","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("D","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("E","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("F","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("G","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("H","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("I","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("J","a",30,60,31,"a",30,12,"06-06-2015"));
+					Voorraadbeheer.medlist.add(new Medicijn("K","a",30,60,31,"a",30,12,"06-06-2015"));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
