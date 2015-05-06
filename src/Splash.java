ca61d325f2e36e4e1045868b73d5a1a40140b324
matsdemeyer <matsdemeyer@gmail.com>
2015-05-06 22:51:57 +0200

    idk

--- src/Splash.java
+++ src/Splash.java
@@ -10,51 +10,7 @@ public class Splash {
 		try {
 			JSplash splash = new JSplash(Main.class.getResource("pharmacy.jpg"), true, true, false, null, null, Color.RED, Color.BLACK);						
 			splash.splashOn();	
-			
-			//Loading Animashun
-//			splash.setProgress(00,"Loading");
-//			Thread.sleep(250);
-//			splash.setProgress(05,"Loading.");
-//			Thread.sleep(250);
-//			splash.setProgress(10,"Loading..");
-//			Thread.sleep(250);
-//			splash.setProgress(15,"Loading...");
-//			Thread.sleep(250);
-//			splash.setProgress(20,"Loading");
-//			Thread.sleep(250);
-//			splash.setProgress(25,"Loading.");
-//			Thread.sleep(250);
-//			splash.setProgress(30,"Loading..");
-//			Thread.sleep(250);
-//			splash.setProgress(35,"Loading...");
-//			Thread.sleep(250);
-//			splash.setProgress(40,"Loading");
-//			Thread.sleep(250);
-//			splash.setProgress(45,"Loading.");
-//			Thread.sleep(250);
-//			splash.setProgress(50,"Loading..");
-//			Thread.sleep(250);
-//			splash.setProgress(55,"Loading...");
-//			Thread.sleep(250);
-//			splash.setProgress(60,"Loading");
-//			Thread.sleep(250);
-//			splash.setProgress(65,"Loading.");
-//			Thread.sleep(250);
-//			splash.setProgress(70,"Loading..");
-//			Thread.sleep(250);
-//			splash.setProgress(75,"Loading...");
-//			Thread.sleep(250);
-//			splash.setProgress(80,"Loading");
-//			Thread.sleep(250);
-//			splash.setProgress(85,"Loading.");
-//			Thread.sleep(250);
-//			splash.setProgress(90,"Loading..");
-//			Thread.sleep(250);
-//			splash.setProgress(95,"Loading...");
-//			Thread.sleep(250);
-//			splash.setProgress(100,"COMPLETE");
-//			Thread.sleep(500);
-			
+				
 			//random text
 			splash.setProgress(0,"Initialising Window");
 			Thread.sleep(500);
@@ -78,9 +34,6 @@ public class Splash {
 			Thread.sleep(500);
 			splash.setProgress(100,"COMPLETE");
 			Thread.sleep(800);
-		
-			splash.setProgress(100,"COMPLETE");
-			Thread.sleep(500);
 			splash.splashOff();			
 			GUI window = new GUI();
 			window.frame.setVisible(true);			
